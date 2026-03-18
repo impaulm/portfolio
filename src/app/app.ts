8588d3bcef6859335db25955432c3ffa5fe9d7a5
Paul Mistou <paul@MacBook-Air-de-Paul.local>
2026-03-18 15:46:41 +0100

    Added professional experience components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header';
 import { AboutMeComponent } from './about-me/about-me';
+import { ExperiencesComponent } from './experiences/experiences';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    ExperiencesComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
