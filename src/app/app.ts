724a5146286944f1f1f1b4c54f55fe8f62d3ec05
Paul Mistou <paul@MacBook-Air-de-Paul.local>
2026-03-18 16:39:46 +0100

    Added education components and modified experiences ones

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header';
 import { AboutMeComponent } from './about-me/about-me';
 import { ExperiencesComponent } from './experiences/experiences';
+import { EducationComponent } from './education/education';
 
 @Component({
   selector: 'app-root',
@@ -9,6 +10,7 @@ import { ExperiencesComponent } from './experiences/experiences';
     HeaderComponent,
     AboutMeComponent,
     ExperiencesComponent,
+    EducationComponent,
   ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
